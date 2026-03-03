00ce6c76473a533083931d26f801da9f5a75f793
Jan Dalheimer <jan@dalheimer.de>
2026-03-03 22:23:20 +0100

    feat: use vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/CatalogPlugin.tsx"),
+      name: "CatalogPlugin",
+      fileName: "catalog-plugin",
+    },
+    rollupOptions: {
+      external: ["Origo"],
+    },
+    sourcemap: true,
+  },
 });
